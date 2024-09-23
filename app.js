ba4f46cd017c495151df29bc47120696568c7b27
abregondo <jazeekylabregondo202300419@gmail.com>
2024-09-23 16:21:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,20 +10,20 @@ function displayInfo(event) {
   let age = document.getElementById("age").value;
   let email = document.getElementById("email").value;
   let hobby = document.getElementById("hobby").value;
-  let birthday = document.getElementById("birthday").value;
+ 
 
   console.log(`Full Name: ${fullName}`);
   console.log(`Age: ${age}`);
   console.log(`Email: ${email}`);
   console.log(`Hobby: ${hobby}`);
-  console.log(`Birthday: ${birthday}`);
+ 
 
   let userInfo = {
     fullName: fullName,
     age: age,
     email: email,
     hobby: hobby,
-    birthday: birthday,
+    
   };
 
   let jsonOutput = `<pre>${JSON.stringify(userInfo, null, 2)}</pre>`;
